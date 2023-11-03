069e1351a765be2a23666b94af4b1f01d7a2bd02
rhysd <lin90162@yahoo.co.jp>
2023-11-03 23:10:48 +0900

    add `insert_delete` fuzzing target

---
+++ fuzz/fuzz_targets/insert_delete.rs
@@ -0,0 +1,26 @@
+#![no_main]
+
+use arbitrary::{Arbitrary as _, Result, Unstructured};
+use libfuzzer_sys::fuzz_target;
+use tui_textarea::{CursorMove, TextArea};
+use tui_textarea_bench::{dummy_terminal, TerminalExt};
+
+fn fuzz(data: &[u8]) -> Result<()> {
+    let mut term = dummy_terminal();
+    let mut textarea = TextArea::default();
+    let mut data = Unstructured::new(data);
+    for i in 0..100 {
+        textarea.move_cursor(CursorMove::arbitrary(&mut data)?);
+        if i % 2 == 0 {
+            textarea.insert_str(String::arbitrary(&mut data)?);
+        } else {
+            textarea.delete_str(usize::arbitrary(&mut data)?);
+        }
+        term.draw_textarea(&mut textarea);
+    }
+    Ok(())
+}
+
+fuzz_target!(|data: &[u8]| {
+    fuzz(data).unwrap();
+});
